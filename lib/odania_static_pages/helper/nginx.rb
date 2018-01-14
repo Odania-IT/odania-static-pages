84ef279f515a38cc8fbf9344636e53e233fddeb9
Mike Petersen <mike@odania-it.de>
2018-01-14 23:06:09 +0100

    Fixed domain extension live/dev

--- lib/odania_static_pages/helper/nginx.rb
+++ lib/odania_static_pages/helper/nginx.rb
@@ -1,9 +1,23 @@
 module NginxHelper
 	def generate_nginx_config(do_rebuild)
+		nginx_default_redirect = @generator_config.nginx_default_redirect
+		if 'live'.eql?(@config.environment)
+			default_vhost_file = 'default-live-vhost.conf.erb'
+			domain_extension = ''
+		else
+			default_vhost_file = 'default-vhost.conf'
+			domain_extension = '.lvh.me'
+
+			puts 'Generating index.html'
+			FileUtils.mkdir_p @config.output_site_path
+			renderer = ERB.new(File.read(File.join(@config.base_dir, 'templates', 'nginx', 'index.html.erb')))
+			File.write File.join(@config.output_site_path, 'index.html'), renderer.result(binding)
+		end
+
 		vhost_renderer = ERB.new(File.read(File.join(@config.base_dir, 'templates', 'nginx', 'vhost.conf.erb')))
 		sites = {}
 		grouped_domains.each_pair do |site_name, page_config|
-			full_site_name = "#{site_name}.lvh.me"
+			full_site_name = "#{site_name}#{domain_extension}"
 			puts "Writing vhost for: #{full_site_name}"
 
 			if @deploy_config.respond_to? :expires
@@ -15,23 +29,11 @@ module NginxHelper
 			FileUtils.mkdir_p @nginx_conf_dir
 			File.write File.join(@nginx_conf_dir, "#{site_name}.conf"), vhost_renderer.result(binding)
 
-			sites[site_name] = "#{site_name}.lvh.me:8080"
+			sites[site_name] = "#{site_name}#{domain_extension}:8080"
 
 			page_config.each do |config|
-				sites["#{site_name}#{config[:baseurl]}"] = "#{site_name}.lvh.me:8080#{config[:baseurl]}"
+				sites["#{site_name}#{config[:baseurl]}"] = "#{site_name}#{domain_extension}:8080#{config[:baseurl]}"
 			end if page_config.count > 1
-		end
-
-		nginx_default_redirect = @generator_config.nginx_default_redirect
-		if 'live'.eql?(@config.environment)
-			default_vhost_file = 'default-live-vhost.conf.erb'
-		else
-			default_vhost_file = 'default-vhost.conf'
-
-			puts 'Generating index.html'
-			FileUtils.mkdir_p @config.output_site_path
-			renderer = ERB.new(File.read(File.join(@config.base_dir, 'templates', 'nginx', 'index.html.erb')))
-			File.write File.join(@config.output_site_path, 'index.html'), renderer.result(binding)
 		end
 
 		puts 'Copy default vhost'
